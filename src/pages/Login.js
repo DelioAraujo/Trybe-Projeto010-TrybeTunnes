5e7a8744b4e835b8739b6336634506867e39582c
DelioAraujo <delioaraujo@gmail.com>
2023-04-13 18:06:15 -0300

    item 1

---
+++ src/pages/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Login extends React.Component {
+  render() {
+    return (
+      <div data-testid="page-login">
+        {/* Conteúdo do componente aqui */}
+      </div>
+    );
+  }
+}
+
+export default Login;
