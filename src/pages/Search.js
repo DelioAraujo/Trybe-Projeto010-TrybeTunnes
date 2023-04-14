46e92c0641541d94e8c0ecc1bf92acb216964d0b
DelioAraujo <delioaraujo@gmail.com>
2023-04-14 10:43:26 -0300

    item 5

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,10 +2,47 @@ import React from 'react';
 import Header from '../meusComponentes/Header';
 
 class Search extends React.Component {
+  state = {
+    pesquisa: '',
+    botaoDesabilitado: true,
+    loading: false,
+  };
+
+  validacaoNome = () => {
+    const { pesquisa } = this.state;
+    const tamanhoMinimo = 2;
+    const validacao = pesquisa.length >= tamanhoMinimo;
+
+    this.setState({
+      botaoDesabilitado: !validacao,
+    });
+  };
+
+  handleChange = (event) => {
+    this.setState({
+      pesquisa: event.target.value,
+    }, this.validacaoNome);
+  };
+
   render() {
+    const { botaoDesabilitado } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        <label htmlFor="search-artist-input">Buscar artista ou banda:</label>
+        <input
+          type="text"
+          id="search-artist-input"
+          data-testid="search-artist-input"
+          onChange={ this.handleChange }
+        />
+        <button
+          disabled={ botaoDesabilitado }
+          type="button"
+          data-testid="search-artist-button"
+        >
+          Pesquisar
+        </button>
       </div>
     );
   }
