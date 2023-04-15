4c21442327541b8deda03b6b1523ce3e26e21f63
DelioAraujo <delioaraujo@gmail.com>
2023-04-15 12:51:56 -0300

    item 9 incompleto

--- src/pages/Album.js
+++ src/pages/Album.js
@@ -3,15 +3,24 @@ import PropTypes from 'prop-types';
 import Header from '../meusComponentes/Header';
 import getMusics from '../services/musicsAPI';
 import MusicCard from '../meusComponentes/MusicCard';
+import { getFavoriteSongs } from '../services/favoriteSongsAPI';
+import Loading from '../meusComponentes/Loading';
 
 class Album extends React.Component {
   state = {
     musicas: [],
     artiste: '',
     album: '',
+    loading: false,
+    listaDeFavoritas: [],
   };
 
   async componentDidMount() {
+    this.requisicaoDaMusica();
+    this.requisicaoFavoritos();
+  }
+
+  requisicaoDaMusica = async () => {
     const {
       match: {
         params: { id },
@@ -27,25 +36,43 @@ class Album extends React.Component {
       artiste: nomeDoArtista,
       album: nomeDoAlbum,
     });
-  }
+  };
+
+  requisicaoFavoritos = async () => {
+    this.setState({
+      loading: true,
+    });
+
+    const listaDeFavoritas = await getFavoriteSongs();
+
+    this.setState({
+      listaDeFavoritas,
+      loading: false,
+    });
+  };
 
   render() {
-    const { musicas, artiste, album } = this.state;
-
+    const { musicas, artiste, album, loading, listaDeFavoritas } = this.state;
     const soMusicas = musicas.slice(1);
 
     return (
       <div data-testid="page-album">
         <Header />
-        <h1 data-testid="artist-name">
-          { artiste }
-        </h1>
-        <h2 data-testid="album-name">
-          { album }
-        </h2>
-        {soMusicas.map((musica) => (
-          <MusicCard key={ musica.trackId } musica={ musica } />
-        ))}
+        {loading ? (
+          <Loading />
+        ) : (
+          <>
+            <h1 data-testid="artist-name">{artiste}</h1>
+            <h2 data-testid="album-name">{album}</h2>
+            {soMusicas.map((musica) => (
+              <MusicCard
+                key={ musica.trackId }
+                musica={ musica }
+                listaDeFavoritas={ listaDeFavoritas }
+              />
+            ))}
+          </>
+        )}
       </div>
     );
   }
